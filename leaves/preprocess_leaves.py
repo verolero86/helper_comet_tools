184440569b89037db5b7164ab8055fd01b912b13
Veronica G. Melesse Vergara <verolero86@users.noreply.github.com>
2021-12-19 02:24:39 -0500

    add conversion of vectors

---
+++ leaves/preprocess_leaves.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import argparse
+
+def read_leaf(fname):
+
+    """
+    Reads a CSV file containing spectra data from a single leaf image.
+    """
+
+    leaf_df = pd.read_csv(fname)
+
+    print(leaf_df.shape)
+    print(leaf_df)
+    
+    leaf_arr = leaf_df.to_numpy()
+    print(leaf_arr.shape)
+
+    # The provided CSV files have a pixel index, a row index, and a column
+    # index
+    labels_index = 3
+
+    # Keep max_pixel number of pixels
+    max_pixel = 10
+    labels = leaf_arr[0:max_pixel,0:labels_index]
+    values = leaf_arr[0:max_pixel,labels_index:]
+
+    # Review collected values
+    print(f"labels = {labels}")
+    print(f"values.shape = {values.shape}")
+    print(f"values = {values}")
+
+
+    # Convert to column vectors
+    vec_values = values.transpose()
+    print(f"vec_values.shape = {vec_values.shape}")
+    print(f"vec_values = {vec_values}")
+
+
+# Main program
+
+if __name__ == "__main__":
+
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Preprocess leaf spectra data for CoMet.')
+    parser.add_argument('--csvfile', required=True, help="The combined CSV file of all Bioclim data.")
+    args = parser.parse_args()
+    #print(args)
+
+    # Store arguments
+    csvfile = args.csvfile
+
+    read_leaf(csvfile)
